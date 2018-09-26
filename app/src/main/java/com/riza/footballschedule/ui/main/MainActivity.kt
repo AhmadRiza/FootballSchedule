fd8ba4f8c45daa420e659fcde19157a69b106038
AhmadRiza <ahmadriza.public@gmail.com>
2018-09-26 13:44:30 +0700

    adapter Added

--- app/src/main/java/com/riza/footballschedule/ui/main/MainActivity.kt
+++ app/src/main/java/com/riza/footballschedule/ui/main/MainActivity.kt
@@ -5,20 +5,31 @@ import android.os.Bundle
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.ArrayAdapter
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.Spinner
+import com.google.gson.Gson
+import com.riza.footballschedule.R
 import com.riza.footballschedule.R.color.colorAccent
+import com.riza.footballschedule.data.ApiRepository
+import com.riza.footballschedule.data.model.Team
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainView {
+
 
     private lateinit var listTeam: RecyclerView
     private lateinit var progressBar: ProgressBar
     private lateinit var swipeRefresh: SwipeRefreshLayout
     private lateinit var spinner: Spinner
+
+    private var teams: MutableList<Team> = mutableListOf();
+    private lateinit var presenter: MainPresenter
+    private lateinit var adapter: MainAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,5 +65,25 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        adapter = MainAdapter(teams)
+        listTeam.adapter = adapter
+        presenter = MainPresenter(this, ApiRepository(), Gson())
+
+        val spinnerItems = resources.getStringArray(R.array.league);
+        val spinnerAdapter = ArrayAdapter(ctx, android.R.layout.simple_spinner_dropdown_item, spinnerItems)
+        spinner.adapter = spinnerAdapter
     }
+
+    override fun showLoading() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hideLoading() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showTeamList(data: List<Team>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
